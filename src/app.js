73630b479058b36c760a8dd5c98ccaa228c52670
SFlorian95 <subramanyam.florian@outlook.fr>
2022-08-26 13:09:44 +0200

    Integration de l'algo de recherche

--- src/app.js
+++ src/app.js
@@ -5,11 +5,20 @@ import { Collapse } from 'bootstrap'
 /* Inject css */
 require('./scss/app.scss')
 
+require('./scripts/pages/index')
+
+const empty = el => {
+  while (el.firstChild) {
+    el.removeChild(el.firstChild)
+  }
+}
+
 const api = require('./scripts/components/api')
 const factoryRecipes = require('./scripts/factories/recipes')
 
 const displayData = async recipes => {
   const section = document.getElementById('recette')
+  empty(section)
 
   recipes.forEach((recipe) => {
     const model = factoryRecipes.create(recipe)
@@ -19,9 +28,25 @@ const displayData = async recipes => {
   })
 }
 
+const isFound = (ingredients, value) => ingredients.find(element => element.ingredient.toLowerCase().includes(value.toLowerCase()))
+
+const searchRecipe = async () => {
+  const searchInput = document.getElementById('search')
+  const input = searchInput.value
+  const recipes = await api.getRecipes()
+  if (input.length >= 3) {
+    const result = recipes.filter(item => item.name.toLowerCase().includes(input.toLowerCase()) ||
+     item.description.toLowerCase().includes(input.toLowerCase()) || isFound(item.ingredients, input))
+    console.log(result)
+    displayData(result)
+  } else {
+    displayData(recipes)
+  }
+}
+
 const init = async () => {
   const data = await api.getRecipes()
-  document.getElementById('search').addEventListener('keyup', api.searchRecipe)
+  document.getElementById('search').addEventListener('input', searchRecipe)
   displayData(data)
 }
 
--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -7,19 +7,6 @@ const url = 'src/data/recipes.json'
  */
 const getRecipes = () => axios.get(url).then(res => res.data.recipes)
 
-const searchRecipe = () => {
-  const searchInput = document.getElementById('search')
-  const input = searchInput.value
-  const recipes = getRecipes()
-  console.log(recipes.name)
-
-  const result = recipes.filter(item => item.name.includes(input))
-  // console.log(result)
-
-  return result
-}
-
 module.exports = {
-  getRecipes,
-  searchRecipe
+  getRecipes
 }
