1bd0b9178375ad0f0f82414d7a747cfa225921f4
SFlorian95 <subramanyam.florian@outlook.fr>
2022-09-01 17:03:27 +0200

    Affichage des items de la dropdown en fonction de la recherche done et integration algorithme pour la recherche avancé

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ import { Collapse } from 'bootstrap'
 /* Inject css */
 require('./scss/app.scss')
 
-require('./scripts/pages/index')
-
 const empty = el => {
   while (el.firstChild) {
     el.removeChild(el.firstChild)
@@ -16,15 +14,34 @@ const empty = el => {
 const api = require('./scripts/components/api')
 const factoryRecipes = require('./scripts/factories/recipes')
 
+const ingredientsAccordion = document.getElementById('ingredients-accordion')
+const applianceAccordion = document.getElementById('appliance-accordion')
+const ustensilsAccordion = document.getElementById('ustensils-accordion')
+
 const displayData = async recipes => {
-  const section = document.getElementById('recette')
-  empty(section)
+  const divRecipes = document.getElementById('recette')
+
+  empty(divRecipes)
+  empty(ingredientsAccordion)
+  empty(applianceAccordion)
+  empty(ustensilsAccordion)
+
+  if (recipes.length === 0) {
+    divRecipes.append('la recherche ne correspond à aucune recettes')
+  }
 
   recipes.forEach((recipe) => {
     const model = factoryRecipes.create(recipe)
     const recipeDom = model.getRecipeCardDom()
 
-    section.appendChild(recipeDom)
+    const ingredientDropdown = model.getDropdownIngredient()
+    const applianceDropdown = model.getDropdownAppliance()
+    const ustensilDropdown = model.getDropdownUstensils()
+
+    divRecipes.appendChild(recipeDom)
+    ingredientsAccordion.appendChild(ingredientDropdown)
+    applianceAccordion.appendChild(applianceDropdown)
+    ustensilsAccordion.appendChild(ustensilDropdown)
   })
 }
 
@@ -37,6 +54,18 @@ const searchRecipe = async () => {
   if (input.length >= 3) {
     const result = recipes.filter(item => item.name.toLowerCase().includes(input.toLowerCase()) ||
      item.description.toLowerCase().includes(input.toLowerCase()) || isFound(item.ingredients, input))
+
+    result.forEach((item) => {
+      const model = factoryRecipes.create(item)
+      const ingredientDropdown = model.getDropdownIngredient()
+      const applianceDropdown = model.getDropdownAppliance()
+      const ustensilDropdown = model.getDropdownUstensils()
+
+      ingredientsAccordion.appendChild(ingredientDropdown)
+      applianceAccordion.appendChild(applianceDropdown)
+      ustensilsAccordion.appendChild(ustensilDropdown)
+    })
+
     console.log(result)
     displayData(result)
   } else {
@@ -44,9 +73,24 @@ const searchRecipe = async () => {
   }
 }
 
+const searchDropdown = async () => {
+  const searchInput = document.querySelector('.dropdown-search')
+  // const searchInput = document.querySelectorAll('.dropdown-search')
+  const recipes = await api.getRecipes()
+
+  const result = recipes.filter(item => item.appliance.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+  //  item.ustensils.map(item => item.toLowerCase().includes(searchInput.value.toLowerCase())) ||
+    isFound(item.ingredients, searchInput.value))
+
+  console.log(result)
+  displayData(result)
+}
+
 const init = async () => {
   const data = await api.getRecipes()
   document.getElementById('search').addEventListener('input', searchRecipe)
+  document.querySelector('.dropdown-search').addEventListener('input', searchDropdown)
+  // document.querySelectorAll('.dropdown-search').forEach((btn) => btn.addEventListener('input', searchDropdown))
   displayData(data)
 }
 
--- src/scripts/factories/recipes.js
+++ src/scripts/factories/recipes.js
@@ -1,6 +1,6 @@
 module.exports = {
   create (data) {
-    const { name, ingredients } = data
+    const { name, ingredients, appliance, ustensils } = data
 
     const getRecipeCardDom = () => {
       const col = document.createElement('div')
@@ -28,6 +28,53 @@ module.exports = {
 
       return (col)
     }
-    return { getRecipeCardDom }
+
+    const getDropdownIngredient = () => {
+      const ul = document.createElement('ul')
+      ul.classList.add('list-group')
+
+      const ingredientsLi = document.createElement('li')
+
+      ingredients.forEach((item) => {
+        ingredientsLi.classList.add('list-group-item')
+        ingredientsLi.textContent = item.ingredient
+      })
+
+      ul.appendChild(ingredientsLi)
+
+      return (ul)
+    }
+
+    const getDropdownAppliance = () => {
+      const ul = document.createElement('ul')
+      ul.classList.add('list-group')
+
+      const applianceLi = document.createElement('li')
+
+      applianceLi.classList.add('list-group-item')
+      applianceLi.textContent = appliance
+
+      ul.appendChild(applianceLi)
+
+      return (ul)
+    }
+
+    const getDropdownUstensils = () => {
+      const ul = document.createElement('ul')
+      ul.classList.add('list-group')
+
+      const ustensilsLi = document.createElement('li')
+
+      ustensils.forEach((item) => {
+        ustensilsLi.classList.add('list-group-item')
+        ustensilsLi.textContent = item
+      })
+
+      ul.appendChild(ustensilsLi)
+
+      return (ul)
+    }
+
+    return { getRecipeCardDom, getDropdownIngredient, getDropdownAppliance, getDropdownUstensils }
   }
 }
